ee34044ac91df2a79fef6af5a8be2397e57eeb8b
Ismail Ibrahim <56676807+Ismail-Ibrahim-Abdinur@users.noreply.github.com>
2021-10-03 07:44:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ root.title("Screen Resolution Detecter")
 root.geometry('500x400')
 root.resizable(width=False, height=False)
 
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+screen_width = root.winfo_screenwidth() # get the screen width
+screen_height = root.winfo_screenheight() # get the screen height
 
-lbl1 = Label(text="Your screen resolution is: ", pady=100, padx=100).pack()
+lbl1 = Label(text="Your screen resolution is: ", pady=100, padx=100).pack() # adding padding
 
-lbl2 = Label(text=str(screen_width) + " x " + str(screen_height), font=(90), borderwidth=1, relief='solid', padx=10, pady=10).pack() # format the integers into strings
+lbl2 = Label(text=str(screen_width) + " x " + str(screen_height), font=(90), borderwidth=1, relief='groove', padx=10, pady=10).pack() # format the integers into strings
 
 root.mainloop()
